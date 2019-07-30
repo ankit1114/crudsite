cbb11e2a314bd0bf8e9c13ec26e110ff96144bb2
ankit <mauryaankit1996@gmail.com>
2019-07-30 14:22:50 +0530

    first commit

--- crudapp/models.py
+++ crudapp/models.py
@@ -3,7 +3,7 @@ from django.utils import timezone
 class Details(models.Model):
     id = models.IntegerField
     name = models.CharField(max_length=100)
-    email = models.EmailField(max_length=100)
-    contact = models.CharField(max_length=15)
+    email = models.emailfield(max_length=100)
+    contact = models.IntegerField(max_length=25)
     class Meta:
-        db_table = "crudapp"
+        db_table = "details"
--- crudapp/urls.py
+++ crudapp/urls.py
@@ -1,11 +1,12 @@
-from django.contrib import admin
-from django.urls import path, include 
+from django.urls import re_path
+from django.contrib.auth import views as auth_views
+from . import views
+
+app_name = 'crudapp'
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('det', views.det),
-    path('show',views.show),
-    path('edit/<int:id>', views.edit),
-    path('update/<int:id>', views.update),
-    path('delete/<int:id>', views.destroy),
+    re_path(r"id/$", auth_views.id.as_view(template_name="accounts/index.html"),name='id'),
+    re_path(r"nmae/$", auth_views.Name.as_view(), name="name"),
+    re_path(r"email/$", views.email.as_view(), name="email"),
+    re_path(r"contact/$", views.Contact.as_view(), name="contact"),
 ]
